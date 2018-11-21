ad31e4ee2335378876a413abd2e6569de8a46cf2
skyexx <44609285+skyexx@users.noreply.github.com>
2018-11-22 01:27:50 +0800

    Update tensorflow7_variable.py

--- tensorflowTUT/tensorflow7_variable.py
+++ tensorflowTUT/tensorflow7_variable.py
@@ -9,11 +9,11 @@ Please note, this code is only for python 3+. If you are using python 2+, please
 from __future__ import print_function
 import tensorflow as tf
 
-state = tf.Variable(0, name='counter')
+state = tf.Variable(0, name='counter') #Tensorflow的变量要用Variable来设置
 #print(state.name)
-one = tf.constant(1)
+one = tf.constant(1) #常量1
 
-new_value = tf.add(state, one)
+new_value = tf.add(state, one) #常量加1个变量还是变量
 update = tf.assign(state, new_value)
 
 # tf.initialize_all_variables() no long valid from
@@ -23,8 +23,8 @@ if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[
 else:
     init = tf.global_variables_initializer()
 
-with tf.Session() as sess:
-    sess.run(init)
+with tf.Session() as sess:    #打开session的一种模式，这种模式不需要sess.close()它会自动关闭
+    sess.run(init)            #一定要run（init），不然你的variables就没初始化
     for _ in range(3):
         sess.run(update)
         print(sess.run(state))
